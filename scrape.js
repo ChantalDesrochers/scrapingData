34835d81112c2ecb8aa15979fc9628b1d6de2b46
Chantal <chandesrochers@gmail.com>
2018-06-28 15:10:21 +0000

    data scraped and in objects

--- scrape.js
+++ scrape.js
@@ -1,6 +1,90 @@
-var request = require('request');
-var cheerio = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
+const Nightmare = require('nightmare');
+const assert = require('assert');
+const express = require('express');
 
+
+
+// // var Nightmare = require('nightmare')
+var nightmare = Nightmare({show: true})
+
+var vo = require('vo');
+
+
+nightmare.goto('https://www.ibiketo.ca/')
+.wait(3000)
+.evaluate(function(){
+  var heads = [];
+  $('h2').each(function(){
+    item = $(this).text()
+    console.log(item)
+    console.log('hello')
+    h2s.push(item)
+  })
+  return heads
+})
+.end()
+.then(function(result){
+  for(h2 in result) {
+    console.log(h2)
+    console.log(result)
+  }
+})
+
+
+// const Nightmare = require('nightmare')
+// const nightmare = Nightmare({ show: true })
+
+// nightmare
+//   .goto('https://duckduckgo.com')
+//   .type('#search_form_input_homepage', 'github nightmare')
+//   .click('#search_button_homepage')
+//   .wait('#r1-0 a.result__a')
+//   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+//   .end()
+//   .then(console.log)
+//   .catch(error => {
+//     console.error('Search failed:', error)
+//   })
+
+// // import Nightmare from 'nightmare';
+
+// const nightmare = Nightmare();
+
+// nightmare.goto('https://www.yelp.com/biz/pai-northern-thai-kitchen-toronto-5')
+//   .evaluate(() => {
+//     return document.title;
+//   })
+//   .end()
+//   .then((title) => {
+//     console.log(title);
+//   })
+// console.log('test')
+// const nightmare = Nightmare({ show: true })
+
+// nightmare
+//   .goto('https://duckduckgo.com')
+//   .type('#search_form_input_homepage', 'github nightmare')
+//   .click('#search_button_homepage')
+//   .wait('#r1-0 a.result__a')
+//   .evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+//   .end()
+//   .then(console.log)
+//   .catch(error => {
+//     console.error('Search failed:', error)
+//   })
+
+// const nightmare = Nightmare();
+
+// nightmare.goto('http://cnn.com')
+//   .evaluate(() => {
+//     return document.title;
+//   })
+//   .end()
+//   .then((title) => {
+//     console.log(title);
+//   })
 
 // Yelp scrape - simple
 // request('https://www.yelp.com/biz/pai-northern-thai-kitchen-toronto-5', function (error, response, html) {
@@ -121,29 +205,52 @@ var cheerio = require('cheerio');
 // });
 
 //all together - issues... full review
-request('https://www.tripadvisor.ca/Restaurant_Review-g155019-d5289181-Reviews-Pai_Northern_Thai_Kitchen-Toronto_Ontario.html', function (error, response, html) {
-  if(!error && response.statusCode == 200) {
-    var $ = cheerio.load(html);
-    var reviewsArray = []
-    var todayDate = new Date().toJSON().slice(0,10)
-    console.log(todayDate)
-    $('.review').each(function(i, el) {
-      var reviewv = $(this).find('p.partial_entry').text()
-      var ratingv = $(this).find('.ui_bubble_rating').attr('class')
-      // var datev = $(this).find('.ratingDate').text()
-      var datev = $(this).find('.ratingDate').attr('title')
-      var namev = $(this).find('.scrname').text()
-      review = {
-        rating: ratingv,
-        author: namev,
-        description: reviewv,
-        datePublished: datev
-      }
-      reviewsArray.push(review)
-    })
-   console.log(reviewsArray)
-  }
-});
+// request('https://www.tripadvisor.ca/Restaurant_Review-g155019-d5289181-Reviews-Pai_Northern_Thai_Kitchen-Toronto_Ontario.html', function (error, response, html) {
+//   if(!error && response.statusCode == 200) {
+//     var $ = cheerio.load(html);
+//     var reviewsArray = []
+//     var todayDate = new Date().toJSON().slice(0,10)
+//     console.log(todayDate)
+//     $('.review').each(function(i, el) {
+//       var reviewv = $(this).find('p.partial_entry').text()
+//       var ratingv = $(this).find('.ui_bubble_rating').attr('class')
+//       // var datev = $(this).find('.ratingDate').text()
+//       var datev = $(this).find('.ratingDate').attr('title')
+//       var namev = $(this).find('.scrname').text()
+//       review = {
+//         rating: ratingv,
+//         author: namev,
+//         description: reviewv,
+//         datePublished: datev
+//       }
+//       reviewsArray.push(review)
+//     })
+//    console.log(reviewsArray)
+//   }
+// });
+
+
+
+// let nightmare = new Nightmare()
+
+// nightmare
+// .useragent(useragent)
+// .goto('https://www.zomato.com/toronto/pai-entertainment-district/reviews')
+// .evaluate(() => {
+//   return document.title;
+// })
+// .end()
+// .then((title) => {
+//   console.log(title);
+// })
+
+// Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36
+
+
+
+
+
+
 
 
 
@@ -155,8 +262,6 @@ request('https://www.tripadvisor.ca/Restaurant_Review-g155019-d5289181-Reviews-P
 
 // options/idea for scraping mulitple pages... function for generating the pages based on input
 // add those items to an object or array
-
-
 
 
 
